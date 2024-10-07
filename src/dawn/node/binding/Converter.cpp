420d9e789fb72dca6f1d5814abc3bc0ae4f17145
Vladimir Davidovich <thy.ringo@gmail.com>
2024-10-07 13:32:09 +0300

    fix feature naming "chromium-experimental-multi-draw-indirect"

--- src/dawn/node/binding/Converter.cpp
+++ src/dawn/node/binding/Converter.cpp
@@ -1518,7 +1518,7 @@ bool Converter::Convert(wgpu::FeatureName& out, interop::GPUFeatureName in) {
         case interop::GPUFeatureName::kSubgroups:
             out = wgpu::FeatureName::Subgroups;
             return true;
-        case interop::GPUFeatureName::kMultiDrawIndirect:
+        case interop::GPUFeatureName::kChromiumExperimentalMultiDrawIndirect:
             out = wgpu::FeatureName::MultiDrawIndirect;
             return true;
         case interop::GPUFeatureName::kDualSourceBlending:
@@ -1557,7 +1557,7 @@ bool Converter::Convert(interop::GPUFeatureName& out, wgpu::FeatureName in) {
         CASE(TextureCompressionETC2, kTextureCompressionEtc2);
         CASE(TimestampQuery, kTimestampQuery);
         CASE(Subgroups, kSubgroups);
-        CASE(MultiDrawIndirect, kMultiDrawIndirect);
+        CASE(MultiDrawIndirect, kChromiumExperimentalMultiDrawIndirect);
         CASE(DualSourceBlending, kDualSourceBlending);
         CASE(ClipDistances, kClipDistances);
         CASE(ChromiumExperimentalImmediateData, kChromiumExperimentalImmediateData);
